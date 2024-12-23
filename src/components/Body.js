371cd5cc6db4c4564b121e04cfca9d494921f94d
Sunil Sharma <sunilsharma.workit@gmail.com>
2024-12-23 17:11:18 +0530

    fix: filter restaurants button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,28 @@
 import SearchBar from "./SearchBar";
 import HotelCard from "./HotelCard";
 import dummyData from "../utils/dummyData";
-import FilterButton from "./FilterButton";
+import { useState } from "react";
 
 let Body = () => {
+  let [hotelList, setHotelList] = useState(dummyData);
   return (
     <>
       <SearchBar />
-      <FilterButton />
+      <button
+        className="filter-btn"
+        onClick={() => {
+          let filteredList = hotelList.filter((hotel) => {
+            return hotel.info.avgRating > 4.2;
+          });
+
+          setHotelList(filteredList);
+        }}
+      >
+        Above 4 star rating
+      </button>
+
       <div className="hotel-container">
-        {dummyData.map((hotel) => (
+        {hotelList.map((hotel) => (
           <HotelCard key={hotel.info.id} hotels={hotel} />
         ))}
       </div>
