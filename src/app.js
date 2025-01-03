79ebf4b8676a8cbd96e5a9c81bea24ffd67bc4fe
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-01-03 17:42:12 +0530

    add dummy hotel menu page

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import ErrorPage from "./components/ErrorPage";
+import HotelMenu from "./components/HotelMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 let Layout = () => {
@@ -32,6 +33,10 @@ const appRouter = createBrowserRouter([
             {
                 path: "/contact",
                 element: <Contact />
+            },
+            {
+                path: "/hotels/:id",
+                element: <HotelMenu />
             }
 
         ],
---
+++ src/components/HotelMenu.js
@@ -0,0 +1,12 @@
+let HotelMenu = ()=> {
+    return (
+        <>
+        <h1>hotel menu</h1>
+        <h3>Item :      price</h3>
+        <h3>Item :      price</h3>
+        <h3>Item :      price</h3>
+        </>
+    )
+}
+
+export default HotelMenu;
