26e516e1156f6d7385dad85908722dde930141f1
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-01-06 19:23:35 +0530

    add dum menu page

--- src/components/HotelMenu.js
+++ src/components/HotelMenu.js
@@ -1,11 +1,46 @@
+import { useEffect, useState } from "react";
+import { MENU_API } from "../utils/constant";
+import ShimmerEffect from "./ShimmerEffect";
+
 let HotelMenu = ()=> {
+
+    let [menuItems, setMenuItems] = useState(null)
+
+    useEffect(()=> {
+        getMenu();
+    },[])
+
+    let getMenu = async ()=> {
+        let data = await fetch(MENU_API);
+        let json = await data.json();
+        setMenuItems(json)
+        // console.log(json?.data?.cards);
+        let hotelName = json?.data?.cards[0].card.card.text
+        let menuItems = json?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card.itemCards;
+
+        // console.log(hotelName);
+        // console.log(menuItems);
+        
+    }
+
+    if(menuItems === null) return <ShimmerEffect />;
+
+    let {text: hotelName} = menuItems?.data?.cards[0].card.card;
+    let {itemCards: menuItemsData} = menuItems?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+
     return (
-        <>
-        <h1>hotel menu</h1>
-        <h3>Item :      price</h3>
-        <h3>Item :      price</h3>
-        <h3>Item :      price</h3>
-        </>
+        <div className="container">
+        <h1>{hotelName}</h1><br />
+        <h3>Menu:</h3><br />
+        
+        <ul>
+            {
+                menuItemsData.map((item)=> {
+                    return <li>{item.card.info.name}</li>
+                })
+            }
+        </ul>
+        </div>
     )
 }
 
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,5 @@
 export const LOGO_URL = "https://img.freepik.com/premium-vector/food-logo-vector-illustration_943898-19509.jpg?semt=ais_hybrid";
 
 export const CDN_LINKS = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const MENU_API = "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.1736225&lng=77.43667049999999&restaurantId=79013&submitAction=ENTER#";
