8546e2196026effb64bb7f54e7d7b4aa92fb31f8
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-02-11 13:59:47 +0530

    add input form in the Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,27 @@
-let Contact = ()=> {
-    return (
-        <h1>Contact</h1>
-    )
-}
+let Contact = () => {
+  return (
+    <div className="mt-20 flex h-96 w-full items-center justify-center">
+      <div className="h-full w-80 px-12 py-12 bg-gray-200 rounded-xl">
+        <h1 className="text-xl font-semibold">Contact us</h1>
+        <form className="flex flex-col gap-y-4 mt-6">
+          <input
+            className="border border-gray-400 px-2 py-1"
+            placeholder="name"
+          ></input>
+          <input
+            className="border border-gray-400 px-2 py-1"
+            placeholder="email"
+          ></input>
+          <input
+            className="border border-gray-400 px-2 py-1"
+            placeholder="message"
+          ></input>
+
+          <button className="self-start bg-green-500 px-4 py-1 text-white rounded">submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
